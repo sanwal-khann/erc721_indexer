9c485b41c2d81c340f14b504e74e686c025042b1
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-03-19 08:59:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { ethers } = require("ethers");
 const fs = require("fs");
 
 async function startListening() {
-  // Ethereum node RPC endpoint
   const rpcUrl =
     "https://eth-mainnet.g.alchemy.com/v2/PdAq5ydIInbxKVSOmwTD5TIYk51FnMXD";
 
@@ -10,21 +9,17 @@ async function startListening() {
   const abiFilePath = "./usdt.json";
   const abi = JSON.parse(fs.readFileSync(abiFilePath, "utf-8"));
 
-  // Contract address
   const contractAddress = "0x6740ce1bDBbFAD351Ec6232faa8c110eBEae36Bf";
 
-  // Create provider for connection
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
-  // Contract instance
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
-  // Event filter for ERC721 Transfer events
+  // Event filter for Transfer events
   const transferFilter = contract.filters.Transfer(null, null, null);
 
-  // Listen for ERC721 transfer events
+  // Listen for  transfer events
   contract.on(transferFilter, async (from, to, tokenId, event) => {
-    // Access all log properties
     console.log("Log Properties:");
     console.log("Address:", event.address);
     console.log("Block Hash:", event.blockHash);
@@ -34,7 +29,6 @@ async function startListening() {
     console.log("Data:", event.data);
     console.log("");
 
-    // Function to handle ERC721 transfer events
     async function handleERC721TransferEvent(from, to, tokenId) {
       // Perform event filtering for self-transfers
       if (from !== to) {
@@ -45,13 +39,10 @@ async function startListening() {
         console.log("");
       }
     }
-
-    // Call the function to handle ERC721 transfer events
     await handleERC721TransferEvent(from, to, tokenId);
   });
 
   console.log("Listening for ERC721 token transfer events ...");
 }
 
-// Start listening for events
 startListening();
